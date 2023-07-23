c9ad573b22007067d2fe0941283a4f737d1d91c4
tadeasf <taddy.fort@gmail.com>
2023-07-23 08:19:54 +0200

    g-tag fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,17 +29,12 @@ module.exports = {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
         trackingIds: [
           'G-F6RSXJ59G6', // Google Analytics / GA
+          // "AW-CONVERSION_ID", // Google Ads / Adwords / AW
         ],
-        // This object gets passed directly to the gtag config command
-        // This config will be shared across all trackingIds
-        gtagConfig: {
-          anonymize_ip: true,
-          cookie_expires: 0,
-        },
-        // This object is used for configuration specific to this plugin
+        gtagConfig: {},
         pluginConfig: {
-          // Puts tracking script in the head instead of the body
           head: true,
+          delayOnRouteUpdate: 0,
         },
       },
     },
